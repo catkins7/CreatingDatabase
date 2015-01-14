ec8383f8b49b38a388e58d79ccdd52224f521a50
catkins7 <craigtatkins@gmail.com>
2015-01-14 16:49:46 -0600

    3rd

--- app/src/main/java/com/example/fatty/creatingdatabase/MainActivity.java
+++ app/src/main/java/com/example/fatty/creatingdatabase/MainActivity.java
@@ -1,42 +1,41 @@
 package com.example.fatty.creatingdatabase;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
 
+
+    SQLiteDatabase contactsDB = null;
+
+    Button createDBButton, addContactButton, deleteContactButton, getContactsButton, deleteDBButton;
+
+    EditText nameEditText, contactListEditText, idEditText, emailEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createDBButton = (Button) findViewById(R.id.createDBButton);
+        addContactButton = (Button) findViewById(R.id.addContactButton);
+        deleteContactButton = (Button) findViewById(R.id.deleteContactButton);
+        getContactsButton = (Button) findViewById(R.id.getContactsButton);
+        deleteDBButton = (Button) findViewById(R.id.deleteDBButton);
+
+        nameEditText = (EditText) findViewById(R.id.nameEditText);
+        contactListEditText = (EditText) findViewById(R.id.contactListEditText);
+        idEditText = (EditText) findViewById(R.id.idEditText);
+        emailEditText = (EditText) findViewById(R.id.emailEditText);
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     public void createDatabbase(View view) {
     }
@@ -48,5 +47,8 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void getContacts(View view) {
+    }
+
+    public void deleteDatabase(View view) {
     }
 }
